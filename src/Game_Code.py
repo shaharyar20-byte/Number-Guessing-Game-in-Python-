3f5841c77c8194375e26adbf068cbfd50aca5daf
shaharyar20-byte <shaharyarsheraz0@gmail.com>
2026-03-18 21:54:10 +0500

    Update Game_Code.py

--- src/Game_Code.py
+++ src/Game_Code.py
@@ -5,7 +5,7 @@ import random
 guess= (eval(input("Enter Your guess between 1-20:    ")))
 guess_number= random.randint(1,20)
 if (guess==guess_number):
-  print("Congratulations",name,"your guess is correct")
+  print("Congratulations! your guess is correct")
   print("You Win")
 elif guess<1 or guess>20:
   print("You guess is out of range. Put guess between 1 and 20")
