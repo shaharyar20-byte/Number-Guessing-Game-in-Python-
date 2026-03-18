a8ed27ef0dce4fb6a4eed87655dba511113f88dd
shaharyar20-byte <shaharyarsheraz0@gmail.com>
2026-03-18 21:53:38 +0500

    Update Game_Code.py

--- src/Game_Code.py
+++ src/Game_Code.py
@@ -1,16 +1,14 @@
 print('  Welcome to Number Guessing Game')
-print("Before the start of game we need following information from you")
-name= str(input("Enter Your Name:  "))
-print("Hello!",name,"Now play your game!")
+print("Hello! Now play your game!")
 
 import random
 guess= (eval(input("Enter Your guess between 1-20:    ")))
 guess_number= random.randint(1,20)
 if (guess==guess_number):
   print("Congratulations",name,"your guess is correct")
-  print(name,"You Win")
+  print("You Win")
 elif guess<1 or guess>20:
   print("You guess is out of range. Put guess between 1 and 20")
 else:
-  print(name,"your guess is wrong")
+  print("your guess is wrong")
   print("You Loss")
